f0b753ba87bd7193ff4b38d47b9851b95ccb768e
davisred <89680207+davisred@users.noreply.github.com>
2022-07-12 19:45:42 +0300

    secret key deleted

--- tododavis/settings.py
+++ tododavis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ui7toykqk42-%h=u7$i0^sa*^nkbcm3_7*0(pzar+$z*myuesw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
